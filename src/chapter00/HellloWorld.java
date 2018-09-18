e11b5f54eacd9b5d53c1fb6bbe9039856762a215
Main Uddin <main06@gmail.com>
2018-09-18 12:04:32 -0400

    First commit on project 1

---
+++ src/chapter00/HellloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * I have copied this  from JavaProgrammingA2Z project
+ */
+package chapter00;
+
+public class HellloWorld {
+	
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+		System.out.println();
+		System.out.print("Welcome to Java Programming");
+		System.out.println();
+		System.out.print("This is my First Java Program");
+		
+		//I am adding this line
+		System.out.println("The shortcut for System.out.println is type sysout and then press ctrl + spaceBar from keyboard");
+		System.out.println();
+		System.out.println("In this way you wont have to type the whole System.out.println");
+		
+	}
+
+}
